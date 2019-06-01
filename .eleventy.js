8811b65067e4aa5c88b96ee462504a1290bc3818
mbestavros <markbest@bu.edu>
2019-06-01 15:36:01 -0400

    Flesh out drawer; experiment with cards on home
    Expand drawer to include header, sections, and meaningful targets (eventual app destinations). Use cards for main view on home. Note: This is the first decent-looking prototype for the finished product.

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,10 @@
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("assets/img");
     return {
         dir: {
             input: "./",      // Equivalent to Jekyll's source property
             output: "./_site" // Equivalent to Jekyll's destination property
         },
+        passthroughFileCopy: true,
     };
 };

b8731152d6762f9d59e999f16525b7f68cf9ac4d
Mark Bestavros <markbest@bu.edu>
2019-10-17 22:20:51 -0400

    Fix components broken by MDC update w/ new syntax

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,12 @@
 // Import MDC components
 import {MDCTopAppBar} from '@material/top-app-bar';
-import {MDCRipple} from '@material/ripple';
 
 // Instantiate elements
 const topAppBarElement = document.getElementById('app-bar');
 const mainContentElement = document.getElementById('main-content');
-const aboutButton = document.getElementById('about-button');
 
 
 
 // Initialize top app bar
 const topAppBar = new MDCTopAppBar(topAppBarElement);
 topAppBar.setScrollTarget(mainContentElement);
-
-//Initialize buttons
-const aboutButtonRipple = new MDCRipple(aboutButton);
